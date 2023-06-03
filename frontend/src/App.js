aad55fda3d19ab2b32101d0f4e66f6b3dd12e66a
Parsa Kargari <parsa.kargari@ucalgary.ca>
2023-06-02 18:39:13 -0600

    FInal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import "./App.css";
+import Diagnosis from "./components/Diagnosis";
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [previewImage, setPreviewImage] = useState(null);
-  const [progress, setProgress] = useState(60); // Initial progress value
+  const [progress, setProgress] = useState(0); // Initial progress value
+  const [diagnosis, setDiagnosis] = useState("No diagnosis yet"); // Initial diagnosis value
+
+  const testData = [
+    //switch according to actual data from backend
+    { bgcolor: "#c6abd6", completed: progress },
+  ];
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -19,21 +26,22 @@ function App() {
     } else {
       setPreviewImage(null);
     }
+    
   };
-
-  
 
   const handleUpload = () => {
     const formData = new FormData();
     formData.append("image", selectedFile);
 
-    fetch("http://localhost:5000/upload", {
+    fetch("http://127.0.0.1:5000/upload", {
       method: "POST",
       body: formData,
     })
       .then((response) => response.json())
       .then((data) => {
         console.log("Response from backend:", data);
+        setProgress(data["result"]);
+        setDiagnosis(data["category"]);
         // Handle the response data here
       })
       .catch((error) => {
@@ -43,6 +51,7 @@ function App() {
 
   return (
     <div className="container">
+      <h1>NeuroSight</h1>
       <div className="uploadBox">
         {previewImage && (
           <img src={previewImage} alt="Preview" className="previewImage" />
@@ -57,9 +66,15 @@ function App() {
           <button onClick={handleUpload} className="uploadButton">
             Upload Image
           </button>
+          <button onClick={handleUpload} className="additionButton">
+            Add Label to Add to our Model
+          </button>
+
         </div>
       </div>
-
+      <h2>
+        Diagnosis: <span id="diagnosis">{diagnosis}</span>
+      </h2>
       <div className="data-output">
         {testData.map((item, idx) => (
           <Diagnosis
